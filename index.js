ef591b4bced300411f48b7080e322fb8084c09b6
Marco Amador <marcoamador214@gmail.com>
2022-10-13 15:37:08 -0700

    commit tree 1151026b8c26434630759610e7f3f22f783fd5c7

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+const dodger = document.getElementById('dodger');
+
+//moves the DODGER to the left
+function moveDodgerLeft() {
+          const leftNumbers = dodger.style.left.replace("px", "");
+          const left = parseInt(leftNumbers, 10);
+      
+          dodger.style.left = `${left - 1}px`;
+}
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'ArrowLeft') {
+        moveDodgerLeft();
+    }
+})
+
+//moves the DODGER to the right
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNumbers, 10);
+
+    dodger.style.left = `${left + 1}px`;
+}
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'ArrowRight') {
+        moveDodgerRight();
+    }
+})
